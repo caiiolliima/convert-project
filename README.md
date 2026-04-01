2d768cb9764904e1d2eaea52b2309c981191b99f
Caio Lima EK <caio.lima@elringklinger.com>
2026-04-01 11:22:13 -0300

    feat: commit readm file and script.js

---
+++ README.md
@@ -0,0 +1,13 @@
+### Convert Project
+
+This project are just educacional, doing with rocketseat school.
+
+### Project ideia
+
+The ideia are learn javascript concepts, fundamentus.
+
+### Concepts
+
+-> Regular expression (Regex) -> used to replace input when the user types any character, replacing it with "none". Input only accepts numbers.
+-> getElementById -> used to get element in HTML, in this case, "amount"
+-> addEventListner -> used to listner every event has changed the input, in this case, "amount"
--- scripts.js
+++ scripts.js
@@ -1,6 +1,43 @@
+const USD = 5.18;
+const EUR = 6.01;
+const GBP = 6.90;
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 amount.addEventListener("input", () =>{
     const hasCharacterRegex = /\D+/g //padrão do regex que busca caracteres no input
     amount.value = amount.value.replace(hasCharacterRegex, "") //retorna o "amount" substituindo os valores caso tenha textos.
 })
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "USD")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "EUR")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "GBP")
+            break
+    }
+}
+
+function convertCurrency(amount, price, symbol) {
+    try {
+        description.textContent = `${symbol} 1 = ${price}`
+        
+        footer.classList.add("show-result")
+    } catch (error) {
+        footer.classList.remove("show-result")
+        console.log(error)
+        alert("Não foi possível converter, tente novamente mais tarde")
+    }
+    
+}
